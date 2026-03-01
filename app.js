fc2ce8bdc89322d7d39f33934aeaaf73cd5bc3ba
kristalolsen1950 <kristal-olsen@live.nmit.co.nz>
2026-03-02 12:01:10 +1300

    Operators

--- app.js
+++ app.js
@@ -12,5 +12,14 @@ let userAge = 30;
 
 console .log("userAge")
 console .log (typeof userAge)
-
-
+// Arithetic
+5 + 3 //8 10-4//6 4*3 //12
+9 / 2 //4.5 9%2 //1 2**3//8
+//Comparison - always prefer === over ==
+5=== //true 5 !==3 // true (stict: checks type + value)
+5 == '5' //true 5 === "5"// false (loose vs strick)
+10>3 //true 4<=4 //true
+//Logical
+true && false //false true || false //true !true //false
+//Assigment shorthands
+let x = 10; x+=5;//15 x-=2;//13 x*=2;//26git
