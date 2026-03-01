aff401a6ad94c421e25ea9a06bbeb54b69bc9198
kristalolsen1950 <kristal-olsen@live.nmit.co.nz>
2026-03-02 11:29:34 +1300

    User Age Test

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log ("hello world")
+
+var name = "Alice"; //function-scoped, hoisted-aviod using var in modern JavaScript
+let age = 25; //block-scoped, can be reassigned 
+const pi = 3.14159; //blocked-scoped, cannot be reassigned 
+let score = 0;
+score = 10; //ok-reassinment allowed for let
+const max = 100;
+//max = 200; //typeerror: assigment to constant variable
+let homeAdress = "123 main road"
+let userAge = 30;
+
+console .log("userAge")
